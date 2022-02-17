ed8e1c8239fac85265ff43fd4b9ca2c1290acbdb
Maria de Lourdes Celeski <marialceleski@gmail.com>
2022-02-17 16:25:05 -0300

    Criando Guids de números randômicos

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MyGuids
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   //quando precisar gerar um valor randômico
+            var id = Guid.NewGuid();
+            id.ToString();
+
+            //passa o valor aqui pra dentro
+            id = new Guid("76e76481-d933-455f-ab80-64241da82e20");
+
+            //if (id == Guid.NewGuid())
+            Console.WriteLine(id.ToString().Substring(0, 8));//O resiltado vai ser os 8 primeiros numeros
+            //usado pra não ficar muito extenso - exemplo = 76e76481 
+
+        }
+    }
+}
